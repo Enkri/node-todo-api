2c4e8a45b3bad29d4e2948b222961f1aa0ec0a46
Chen Lin <Enkri_@macbook-pro.lems.brown.edu>
2017-06-12 16:14:33 -0400

    set up heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,8 +5,7 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://Enkri_:qwertyuiop@ds149268.mlab.com:49268/todoapp'
 };
-mongoose.connect( db.localhost || db.mlab);
+mongoose.connect(db.mlab || db.localhost);
 module.exports = {
   mongoose: mongoose,
 }
- 
