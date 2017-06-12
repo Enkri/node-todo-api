d38c72128e047c2f3aa0a203431d1a75fba35665
Chen Lin <Enkri_@macbook-pro.lems.brown.edu>
2017-06-12 16:23:44 -0400

    config mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // set promise
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://Enkri_:qwertyuiop@ds149268.mlab.com:49268/todoapp'
-};
-mongoose.connect(db.mlab || db.localhost);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 module.exports = {
   mongoose: mongoose,
 }
