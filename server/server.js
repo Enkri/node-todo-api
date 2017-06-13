ea773ee445f08267c460a4320132db51ef37209e
Chen Lin <Enkri_@macbook-pro.lems.brown.edu>
2017-06-13 16:43:20 -0400

    Add PATCH /todos:id

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
   if (ObjectId.isValid(id)) {
     Todo.findById(id).then((todo) => {
       if (!todo) {
-        return res.status(404).send();
+        res.status(404).send().end();
       }
       res.send({todo: todo});
     }).catch((e) => {
@@ -72,32 +72,7 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
-// app.patch('/todos/:id', (req, res) =>{
-//   var id = req.params.id;
-//   var body = _.pick(req.body, ['text', 'completed']);
-//
-//   if (!ObjectId.isValid(id)) {
-//     return res.status(404).send();
-//   }
-//
-//   if (_.isBoolean(body.completed) && body.completed) {
-//     body.completedAt = new Data().getTime();
-//   } else {
-//     body.completed = false;
-//     body.completedAt = null;
-//   }
-//
-//   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-//     if (!todo) {
-//       return res.status(404).send();
-//     }
-//     res.send({todo});
-//   }).catch((e) => {
-//     res.status(400).send();
-//   })
-//
-// });
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', (req, res) =>{
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -116,12 +91,13 @@ app.patch('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   })
+
 });
+
 
 
 app.listen(port, () => {
